429f3c9953a7cde2323ba1a5ca0d3a51c006d69f
alifhamdan <alifhamdanrifai@gmail.com>
2018-10-02 15:31:31 +0700

    push

---
+++ break and continou.php
@@ -0,0 +1,32 @@
+<?php
+
+// =========================break========================
+
+// $kru = ['alif', 'towi', 'bintang', 'rizal', 'naufal'];
+//
+// foreach ($kru as $key => $nama) {
+//   echo "Nama : $nama" ."<br>";
+//     if ($nama == 'bintang') {
+//     echo "bintang ada di index .$key ";
+//     break;
+//   }
+// }
+
+
+
+// =========================continue=============================================
+// $siswa = [
+//   ['Nama' => 'sulip', 'kelas' => '2A' ],
+//   ['Nama' => 'golep', 'kelas' => '2B' ],
+//   ['Nama' => 'towi', 'kelas' => '2C' ],
+//   ['Nama' => 'hafiz', 'kelas' => '2D' ],
+// ];
+// foreach ($siswa as $key => $value) {
+//   if ($value['kelas'] == "2A")
+//     continue;
+//     echo "Nama :" .$value['Nama'] . "<br>";
+//     echo "kelas :" .$value['kelas'] . "<br> <br>";
+// }
+
+
+ ?>
